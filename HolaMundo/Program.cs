77022fb32d0e32dcd963016526fa9af3dc135bda
Elvis Geovanny <elvis.batzibal@hotmail.es>
2019-12-17 06:41:02 -0600

    definition de main class

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HolaMundo
+﻿
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
+
