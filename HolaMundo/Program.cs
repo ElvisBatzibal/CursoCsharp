196d4f6729e7836bc9f85d326307de5e78adacac
Elvis Geovanny <elvis.batzibal@hotmail.es>
2019-12-18 21:15:15 -0600

    Variables primitivas, declaracion e inicialización

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -18,6 +18,41 @@ class Program
 
         //Control K +Control C Para Comentar
         //Control K +Control U Para Descomentar
+
+        //Declaracion de Variables
+        //Tipos de datos primitivos: Más comunes
+        //https://www.dotnetperls.com/
+
+        //Declaracion de variables
+        //numeros enteros. Tamaño: 32
+        int count;
+        //numeros enteros - bigger range: Tamaño 64
+        long wait;
+        //numero de puntos flotantes: Tamaño: 32
+        float away;
+        //numeros de punto flotante de Doble-Precisión. Más precisos: Tamaño:64
+        double trouble;
+        //valore monetarios: Tamaños 128
+        decimal coin;
+        //Secuencia de caracteres: Tamaño: 16 bits por caracter
+        string vest;
+        //Caracter unico: Tamaño 16
+        char grill;
+        //Boleanos: Tamaño: 8
+        bool teeth;
+
+        //Inicializacion de variables
+        count = 42;
+        wait = 42L;
+        away = 0.42F;
+        trouble = 0.42;
+        coin = 0.42M;
+        vest = "Forty";
+        grill = 'x';
+        teeth = false;
+
+        //Información adicional invetigar el rango de valores 
+
     }
 }
 
