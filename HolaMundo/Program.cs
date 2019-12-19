8b4b1f088736bed29c2163875ec3fbc975c5b0b5
Elvis Geovanny <elvis.batzibal@hotmail.es>
2019-12-18 20:51:06 -0600

    Tipos de comentarios

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -5,6 +5,16 @@ class Program
     {
         Console.WriteLine("Hello World");
         Console.ReadLine();
+
+        //Comentario Simple-Linea:Funcional en una sola linea
+        Console.WriteLine(5 + 2);
+        Console.WriteLine("Hector");
+
+        //Comentario Multilinea
+        /* Inicio
+         Console.WriteLine("Hector");
+         Console.WriteLine("Hector");
+         */
     }
 }
 
